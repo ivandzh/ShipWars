042435f669d9c73277076221d4e2b17b5ef44957
Ivan D <ivan.dzh93@gmail.com>
2016-04-27 10:02:50 +0300

    FIXING still

--- src/js/entities/player.js
+++ src/js/entities/player.js
@@ -81,8 +81,8 @@ var Player = function (player) {
     this.emitterTwo.minParticleSpeed = new Phaser.Point(-10,-100);
 
     //swap emitter with player, place underneath
-    player.game.world.swap(emitterOne, player);
-    player.game.world.swap(emitterTwo, player);
+    player.game.world.swap(this.emitterOne, player);
+    player.game.world.swap(this.emitterTwo, player);
 
     this.playerController();
 };
