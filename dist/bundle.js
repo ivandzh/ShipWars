5c1f8c7c234df27948a35631b051a79e80b00e48
Ivan D <ivan.dzh93@gmail.com>
2016-04-29 13:26:45 +0300

    still fixing the z-index of the player sprite

--- dist/bundle.js
+++ dist/bundle.js
@@ -430,12 +430,10 @@ var Player = require('../entities/player');
 var Game = function () {
 
     this.players = null;
-    //this.rocks = null;
+    this.rocks = null;
     this.barrels = null;
     console.log("Play stage initiated");
-    this.deathCounter = 0;
     this.playersAlive = 0;
-    this.winnerData = null;
 };
 
 Game.prototype = {
@@ -459,11 +457,6 @@ Game.prototype = {
 
         for (var i = 0; i < this.players.children.length; i++) { // for each player
 
-            this.winnerData = {
-                id: this.players.children[i].playerId,
-                num:this.players.children[i].playerNum
-            };
-
             //play the emitters
             this.players.children[i].emitterOne.emitParticle();
             this.players.children[i].emitterTwo.emitParticle();
@@ -477,9 +470,6 @@ Game.prototype = {
             //in case of player hitting barrel, destroy barrel and player
             this.game.physics.arcade.overlap(this.players.children[i], this.barrels, function (player, barrel) {
 
-                //player.kill();
-                //barrel.kill();
-
                 var tweenB = gameObj.game.add.tween(barrel);
                 tweenB.to( { alpha: 0 }, 100, Phaser.Easing.Linear.None);
                 tweenB.onComplete.add(function () {
@@ -491,7 +481,6 @@ Game.prototype = {
                 tweenP.to( { alpha: 0 }, 100, Phaser.Easing.Linear.None);
                 tweenP.onComplete.add(function () {
                     player.kill();
-                    gameObj.deathCounter++;
                 });
                 tweenP.start();
 
@@ -507,7 +496,7 @@ Game.prototype = {
            this.game.physics.arcade.overlap(this.players.children[i].lasers, this.barrels, function (laser, barrel) {
 
                laser.kill();
-               //barrel.kill();
+
                 var tween = gameObj.game.add.tween(barrel);
                     tween.to( { alpha: 0 }, 100, Phaser.Easing.Linear.None);
                     tween.onComplete.add(function () {
@@ -525,16 +514,12 @@ Game.prototype = {
             //in case of player hitting rock, destroy only player
             this.game.physics.arcade.overlap(this.players.children[i], this.rocks, function (player) {
 
-                //player.kill();
                 var tweenP = gameObj.game.add.tween(player);
                 tweenP.to( { alpha: 0 }, 100, Phaser.Easing.Linear.None);
                 tweenP.onComplete.add(function () {
                     player.kill();
-                    gameObj.deathCounter++;
                 });
                 tweenP.start();
-
-                //this.game.add.tween(player).to( { alpha: 0 }, 1000, Phaser.Easing.Linear.None, true);
 
                 gameObj.explodeAudio.play(); //explode
                 //add explosion sprite
@@ -553,33 +538,24 @@ Game.prototype = {
                 if (this.players.children[i].playerId === this.players.children[j].playerId) {
                     continue; //skip if player is the exact same
                 }
-                var winnerData = {
-                    //player: this.players[i],
-                    id: this.players.children[i].playerId,
-                    num:this.players.children[i].playerNum
-                };
+
                 this.game.physics.arcade.overlap(this.players.children[i].lasers, this.players.children[j], function (laser, player) {
+
                     laser.kill();
-                    //player.kill();
 
                     var tweenP = gameObj.game.add.tween(player);
                     tweenP.to( { alpha: 0 }, 100, Phaser.Easing.Linear.None);
                     tweenP.onComplete.add(function () {
                         player.kill();
-                        gameObj.deathCounter++;
-                        gameObj.playersAlive--;
 
                         //Check if there is only one player alive, if yes - move to win state.
-                        //console.log("Winner lasers " + winnerData.player.lasers + " =?= " + laser);
-                        //if (winnerData.player.lasers == laser) {
-                           // if (gameObj.playersAlive == 1 && gameObj.deathCounter >= 1)
                             if (gameObj.players.countLiving() == 1 && gameObj.players.countDead() >= 1)
                             {
                                 console.log("We have a winner!");
                                 console.log(gameObj.players.countDead());
                                 console.log(gameObj.players.countLiving());
 
-                                //USE getFirstAlive(), only one will be alive, will return object, use id, compare with shooter
+                                //USE getFirstAlive(), only one will be alive, will return object
                                 var lastManStanding = gameObj.players.getFirstAlive();
                                 console.log(lastManStanding);
                                 var winnerData = {
@@ -587,18 +563,12 @@ Game.prototype = {
                                     num : lastManStanding.playerNum
                                 };
                                 Sockets.emit("server player win", winnerData);
-                                for (var i = 0; i < gameObj.players.children.length; i++) {
-                                        gameObj.players.children[i].destroy(true);
-                                        //gameObj.players.splice(i, 1);
-                                        //gameObj.players = [];
-                                }
                                 gameObj.game.cache.removeSound('inGameLoop');
                                 gameObj.game.state.start('Win'); // move to win state*/
                             }
-                       // }
                     });
                     tweenP.start();
-                    gameObj.playerHitAudio.play(); //explode
+                    gameObj.explodeAudio.play(); //explode
                     //add explosion sprite
                     /*var playerExpl = player.explosions.getFirstExists(false);
                     playerExpl.reset(player.x, player.y);
@@ -614,15 +584,11 @@ Game.prototype = {
     //set the Arena
     setArena : function () {
         this.game.add.tileSprite(0, 0, screen.width, screen.height, "backgroundWater");
-        this.game.renderer.clearBeforeRender = true; // difference ??
+        this.game.renderer.clearBeforeRender = true;
         this.game.renderer.roundPixels = true;
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        //this.playingAudio = this.game.add.audio("titleSequence"); //titleSequence  //AUDIO SHOULD BE FIXED
-        //this.playingAudio.volume = 0.6;
-        //this.playingAudio.loop = true; //false?
-        //this.playingAudio.play();
 
-        this.explodeAudio = this.game.add.audio('explode'); //explode    DOUBLE
+        this.explodeAudio = this.game.add.audio('explode');
     },
 
     setPlayers : function () {
@@ -630,8 +596,7 @@ Game.prototype = {
         var gameObj = this;
 
         this.players = this.game.add.group();
-
-        this.playerHitAudio = this.game.add.audio("explode"); //explode
+        this.gameLayers.playerLayer.add(this.players);
 
         Sockets.on("client new player", function (data) {
             gameObj.playersAlive++; //add one to the counter of players alive
@@ -647,13 +612,12 @@ Game.prototype = {
         });
 
         Sockets.on("client disconnected", function (data) {
-            for (var i = 0; i < gameObj.players.length; i++) {
+            for (var i = 0; i < gameObj.players.children.length; i++) {
                 if (gameObj.players.children[i].playerId === data.id) {
                     gameObj.players.children[i].destroy(true);
                     //gameObj.players.splice(i, 1);
                 }
             }
-            console.log("client disconnected"); //gets triggered before connecting new player
         });
     },
 
